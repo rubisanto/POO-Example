ae33d436bd4da86dd07d0ef3850bd33a9e7bf43a
rubisanto <antoinegogceapb@hotmail.fr>
2022-05-18 20:14:01 +0200

    Heritage

--- introduction.php
+++ introduction.php
@@ -6,7 +6,12 @@ class Employe
     public $nom;
     public $prenom;
     // Pour l'encapsulation insérer un private pour l'imiter l'accès à la variable
-    private $age;
+    // private $age;
+
+    // Pour réserver la modification mettre en protected
+    // comme le private mais la modification est possible pour les class qui héritent d Employe
+    protected $age;
+
 
     // Maintenant l'age est disponible que dans les fonctions déclarées de la class
 
@@ -21,6 +26,7 @@ class Employe
     }
 
     // getter et setter pour pouvoir travailler sur une fonction private 
+
     //set pour pouvoir la modifier
     public function setAge($age)
     {
@@ -41,7 +47,7 @@ class Employe
 
     public function presentation()
     {
-        var_dump("Bonjour, je suis $this->prenom $this->nom et j'ai $this->age ans");
+        var_dump("Salut, je suis $this->prenom $this->nom et j'ai $this->age ans");
     }
 }
 
@@ -55,3 +61,46 @@ $employe1->presentation();
 
 //pour modifier l'age il faut faire 
 $employe1->setAge(50);
+
+// ajout d'une classe supplémentaire 
+
+
+// Heritage avec l extends
+// Patron est enfant de la classe parent Employe
+class Patron extends Employe
+{
+
+    private $voiture;
+    // Maintenant l'age est disponible que dans les fonctions déclarées de la class
+
+
+    // Utiliser un constructeur 
+    public function __construct($prenom, $nom, $age, $voiture)
+    {
+        // Pour éviter de réécrire tout le construct
+
+        parent::__construct($prenom, $nom, $age);
+
+
+        $this->voiture = $voiture;
+    }
+
+    // Redéfinition de la méthode présentation pour qu'elle soit différente de la classe parent 
+    public function presentation()
+    {
+        var_dump("Bonjour, je suis $this->prenom $this->nom et j'ai $this->age ans, je roule en $this->voiture et je suis le patron");
+    }
+
+
+    // Nouvelle fonction sépcifique à la nouvelle class
+
+    public function conduire()
+    {
+        var_dump("Je roule en $this->voiture");
+    }
+}
+
+
+$patron = new Patron("joseph", "roland", 51, "mercedes");
+$patron->presentation();
+$patron->conduire();
